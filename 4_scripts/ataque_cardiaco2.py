78e99c5ce9f87d788cfed9c501d5144cd99abb6c
Natan Cabral <102121764+Ntzim@users.noreply.github.com>
2024-04-19 21:56:32 -0300

    Update ataque_cardiaco2.py

--- 4_scripts/ataque_cardiaco2.py
+++ 4_scripts/ataque_cardiaco2.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import streamlit as st
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+import numpy as np
+import joblib
+
+
+# Técnicas
+from sklearn.ensemble import GradientBoostingClassifier as GBC
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.neighbors import KNeighborsClassifier as KNC
+
+# Validação cruzada
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.metrics import classification_report
 
 # carregando dados
-df = pd.read_csv('heart.csv')
+df = pd.read_csv('../heart.csv')
 df['Sex'] = df['Sex'].replace({'F': 0, 'M': 1})
 
 #Titulo do Site
@@ -23,6 +32,9 @@ st.write("Paciente:", user_input)
 
 #Criando função para coletar os dados do paciente
 def get_user_data():
+
+
+
     # Input Idade
     age = st.sidebar.number_input("Idade", 18, 100, 25)
 
@@ -69,14 +81,14 @@ def get_user_data():
         'Age': age,
         'Sex': sex,
         'ChestPainType': chest_pain_type,
-        'RestingBloodPressure': resting_blood_pressure,
+        'RestingBP': resting_blood_pressure,
         'Cholesterol': cholesterol,
-        'FastingBloodSugar': fasting_blood_sugar,
+        'FastingBS': fasting_blood_sugar,
         'RestingECG': resting_ecg,
-        'MaxHeartRateAchieved': max_heart_rate_achieved,
-        'ExerciseInducedAngina': exercise_induced_angina,
-        'STDepression': st_depression,
-        'STSlope': st_slope
+        'MaxHR': max_heart_rate_achieved,
+        'ExerciseAngina': exercise_induced_angina,
+        'Oldpeak': st_depression,
+        'ST_Slope': st_slope
     }
 
     # Convertendo Dicionario em Dataframe
@@ -84,39 +96,73 @@ def get_user_data():
 
     return user_data_df
 
-#treinando modelos
 
-# user_input_variables = get_user_data()
-
-# x = df.drop(columns =['HeartDisease','ChestPainType','RestingECG',
-#                         'ExerciseAngina','ST_Slope'],axis=1)
-
-# y = df['HeartDisease']
-
-# x_train, x_text, y_train, y_test = train_test_split(x, y, test_size=0.2)
-
-# dtc = DecisionTreeClassifier(criterion='entropy', max_depth=3)
-
-# dtc.fit(x_train, y_train)
-
-# #acurácia do modelo
-
-# st.subheader('Acurácia do modelo')
-
-# st.write(accuracy_score(y_test, dtc.predict(x_text))*100)
-
-
-# #previsão
-# prediction = dtc.predict(user_input_variables)
-
-# st.subheader('Previsão:')
-
-# st.write(prediction)
-
-# st.write(prediction)
 
 # Transformando funcao get_user_data em variavel
-user_data = get_user_data()
+df = get_user_data()
+
+
+# PRÉ-PROCESSAMENTO DOS DADOS
+
+# Age
+df['PRE_AGE'] = [34  if x < 34 or np.isnan(x) else x for x in df['Age']]
+df['PRE_AGE'] = [74 if x > 74 else x for x in df['PRE_AGE']]
+df['PRE_AGE'] = [(x-34)/(74-34) for x in df['PRE_AGE']]
+
+# Sex
+df['PRE_SEX_F'] =   [1 if x=='F' else 0 for x in df['Sex']]
+df['PRE_SEX_M'] =   [1 if x=='M' else 0 for x in df['Sex']]
+
+# ChestPainType
+df['PRE_CPT_ASY'] =   [1 if x=='ASY'   else 0 for x in df['ChestPainType']]
+df['PRE_CPT_ATA'] =   [1 if x=='ATA'   else 0 for x in df['ChestPainType']]
+df['PRE_CPT_NAP'] =   [1 if x=='NAP'   else 0 for x in df['ChestPainType']]
+df['PRE_CPT_TA'] =   [1 if x=='TA'   else 0 for x in df['ChestPainType']]
+
+# RestingBP
+df['PRE_RESTINGBP'] = [95  if x < 95 or np.isnan(x) else x for x in df['RestingBP']]
+df['PRE_RESTINGBP'] = [185  if x > 185 else x for x in df['PRE_RESTINGBP']]
+df['PRE_RESTINGBP'] = [(x-95)/(185-95) for x in df['PRE_RESTINGBP']]
+
+# Cholesterol
+df['PRE_CHOLESTEROL'] = [100  if x < 100 or np.isnan(x) else x for x in df['Cholesterol']]
+df['PRE_CHOLESTEROL'] = [250  if x > 250 else x for x in df['PRE_CHOLESTEROL']]
+df['PRE_CHOLESTEROL'] = [(x-100)/(250-100) for x in df['PRE_CHOLESTEROL']]
+
+# FastingBS
+df['PRE_FBS_0'] =   [1 if x==0 else 0 for x in df['FastingBS']]
+df['PRE_FBS_1'] =   [1 if x==1 else 0 for x in df['FastingBS']]
+
+# RestingECG
+df['PRE_RESTECG_LVH'] =   [1 if x=='LVH'   else 0 for x in df['RestingECG']]
+df['PRE_RESTECG_NORMAL'] =   [1 if x=='Normal'   else 0 for x in df['RestingECG']]
+df['PRE_RESTECG_ST'] =   [1 if x=='ST'   else 0 for x in df['RestingECG']]
+
+# MaxHR
+df['PRE_MAXHR'] = [60  if x < 60 or np.isnan(x) else x for x in df['MaxHR']]
+df['PRE_MAXHR'] = [195  if x > 195 else x for x in df['PRE_MAXHR']]
+df['PRE_MAXHR'] = [(x-60)/(195-60) for x in df['PRE_MAXHR']]
+
+# ExerciseAngina
+df['PRE_ANGINA_N'] =   [1 if x=='N' else 0 for x in df['ExerciseAngina']]
+df['PRE_ANGINA_Y'] =   [1 if x=='Y' else 0 for x in df['ExerciseAngina']]
+
+# Oldpeak
+df['PRE_OLDPEAK_ECG'] = [-0.6  if x < -0.6 or np.isnan(x) else x for x in df['Oldpeak']]
+df['PRE_OLDPEAK_ECG'] = [2.4  if x > 2.4 else x for x in df['PRE_OLDPEAK_ECG']]
+df['PRE_OLDPEAK_ECG'] = [(x-(-0.6))/(2.4-(-0.6)) for x in df['PRE_OLDPEAK_ECG']]
+
+# ST_Slope
+df['PRE_STSLOPE_FLAT'] =   [1 if x=='Flat'   else 0 for x in df['ST_Slope']]
+df['PRE_STSLOPE_UP'] =   [1 if x=='Up'   else 0 for x in df['ST_Slope']]
+df['PRE_STSLOPE_DOWN'] =   [1 if x=='Down'   else 0 for x in df['ST_Slope']]
+
+
+
+# CARREGANDO MODELO
+modelo = joblib.load('../4_scripts/modelo_treinado_gradientboosting.pk')
+
+
 
 # Plotando grafico
-chart = st.bar_chart(user_data)
+chart = st.bar_chart(df)
